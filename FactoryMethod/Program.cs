029471a053c19672dfffa506b5f8e79a16a7d46a
Kontorta16 <75789956+Kontorta16@users.noreply.github.com>
2021-02-01 22:47:03 +0300

    Add project files.

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(new LoggerFactory2());
+            customerManager.Save();
+        }
+    }
+
+    public class LoggerFactory:ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            //Business to decide factory!
+
+            return new EmircanLogger();
+        }
+    }
+    public class LoggerFactory2 : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            //Business to decide factory!
+
+            return new Log4NetLogger();
+        }
+    }
+
+    public interface ILoggerFactory
+    {
+        ILogger CreateLogger();
+    }
+
+    public interface ILogger
+    {
+        void Log();
+    }
+
+    public class EmircanLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged With EmircanLogger");
+        }
+    }
+    public class Log4NetLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged With Log4Net");
+        }
+    }
+
+    public class CustomerManager
+    {
+        private ILoggerFactory _loggerFactory;
+
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved!");
+            ILogger logger = _loggerFactory.CreateLogger();
+            logger.Log();
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerManager = CustomerManager.CreateAsSingleton();
+            customerManager.Save();
+        }
+    }
+
+    class CustomerManager
+    {
+        //Private yaptık dışarıdan erişilemez diye
+        private static CustomerManager _customerManager;
+        private static object _lockObject = new object();
+        private CustomerManager()
+        {
+
+        }
+
+        //Kendisini döndürür
+        public static CustomerManager CreateAsSingleton()
+        {
+            lock (_lockObject) { return _customerManager ??= new CustomerManager(); }
+        }
+        public void Save()
+        {
+            Console.WriteLine("Kayıt Başarılı");
+        }
+    }
+}
