aae76aa6f65f7faef209f80ee27943ace1b8ec15
Kontorta16 <75789956+Kontorta16@users.noreply.github.com>
2021-02-02 11:35:38 +0300

    v1.1

---
+++ Prototype/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Clone üzerinden gider ve performans anlamında iyidir
+            Customer customer1 = new Customer { FirstName = "Emircan", LastName = "Dalman", City = "Giresun", Id = 1 };
+            Console.WriteLine(customer1.FirstName);
+
+            Customer customer2 = (Customer)customer1.Clone();
+            customer2.FirstName = "Ali";
+            customer2.LastName = "Dalman";
+            customer2.City = "Giresun";
+            customer2.Id = 2;
+            Console.WriteLine(customer2.FirstName);
+        }
+    }
+
+    public abstract class Person
+    {
+        public abstract Person Clone();
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    public class Customer : Person
+    {
+        public string City { get; set; }
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+    public class Employee : Person
+    {
+        public decimal Salary { get; set; }
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+}
